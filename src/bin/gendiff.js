7256db592595bf6d6b3a3ef51a4f72327fbcd35a
amalker <it.amalker@yandex.ru>
2020-01-21 19:31:13 +0300

    add format option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,6 +3,23 @@ import program from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-V, --version', 'output the version number');
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'Output format')
+  .action(() => {
+    const { format } = program;
+    const outputFormats = ['json', 'plain'];
+    const isCorrectFormat = outputFormats.includes(format.toLowerCase());
+    if (isCorrectFormat) {
+      console.log('Format type:', format);
+    } else {
+      console.error(`incorrect format type, use [${outputFormats.join(', ')}]`);
+      process.exit(1);
+    }
+  })
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log('First config path:', firstConfig);
+    console.log('Second config path:', secondConfig);
+  });
 
 program.parse(process.argv);
