05436ae6af4e5788aaf2fbaf3903ff7a4c53159f
amalker <it.amalker@yandex.ru>
2020-01-23 21:17:41 +0300

    add ini parcer

--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,23 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
 import _ from 'lodash';
+import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (pathToFile) => {
   const formats = {
     json: { extension: '', parserType: JSON.parse },
     yaml: { extension: '.yml', parserType: yaml.safeLoad },
+    ini: { extension: '.ini', parserType: ini.parse },
   };
   const fileExtension = path.extname(pathToFile);
-  const data = fs.readFileSync(pathToFile);
+  const data = fs.readFileSync(pathToFile, 'utf-8');
   const fileFormat = _.findKey(formats, ['extension', fileExtension]);
   const parser = formats[fileFormat].parserType;
   const result = parser(data);
+  if (fileExtension === '.ini') {
+    return Object.values(result).reduce((acc, value) => Object.assign(acc, value), {});
+  }
 
   return result === null ? '' : result;
 };
