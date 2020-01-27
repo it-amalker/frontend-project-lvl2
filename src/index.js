4bfb8538444ba2ba535979f205774791c9891e11
amalker <it.amalker@yandex.ru>
2020-01-27 15:39:55 +0300

    refactor render func

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 import parse from './parsers';
 
+const isObject = (...items) => items.every((obj) => obj instanceof Object);
+const isArray = (...items) => items.every((arr) => Array.isArray(arr));
+
 const diff = (object1, object2) => {
   const commonKeys = _.uniq(Object.keys(object1).concat(Object.keys(object2)));
 
-  const result = commonKeys.sort().reduce((acc, key) => {
+  const ast = commonKeys.sort().reduce((acc, key) => {
     const value1 = object1[key];
     const value2 = object2[key];
     const buildAstNode = (obj1, obj2) => {
@@ -17,19 +20,17 @@ const diff = (object1, object2) => {
         status: '',
       };
       const compareArray = (arr1, arr2) => arr1.every((el) => arr2.includes(el));
-      const isObjects = (...items) => items.every((obj) => obj instanceof Object);
-      const isArrays = (...items) => items.every((arr) => Array.isArray(arr));
       const isKeyInObj1 = _.has(obj1, key);
       const isKeyInObj2 = _.has(obj2, key);
 
       if (isKeyInObj1 && isKeyInObj2) {
-        if (isObjects(value1, value2) && !isArrays(value1, value2)) {
+        if (isObject(value1, value2) && !isArray(value1, value2)) {
           node.status = 'unchanged';
           node.type = 'children';
         } else {
           node.value = value1;
           node.valuePrev = value2;
-          if (isArrays(value1, value2)) {
+          if (isArray(value1, value2)) {
             node.status = compareArray(value1, value2) ? 'unchanged' : 'changed';
           } else {
             node.status = node.value === node.valuePrev ? 'unchanged' : 'changed';
@@ -48,7 +49,6 @@ const diff = (object1, object2) => {
     };
 
     const astNode = buildAstNode(object1, object2);
-
     if (!acc[key]) {
       acc[key] = [];
     }
@@ -62,48 +62,61 @@ const diff = (object1, object2) => {
     return acc;
   }, {});
 
-  return result;
+  return ast;
 };
 
 const render = (ast) => {
-  const iter = (tree, depth) => {
+  const iterAst = (tree, depth) => {
     const currentDepth = depth + 1;
-    const res = Object.entries(tree).reduce((acc, [key, value]) => {
-      const mapStatus = {
-        unchanged: ' '.repeat(4 * currentDepth),
-        removed: `${' '.repeat(currentDepth === 1 ? 2 : 3 * currentDepth + currentDepth - 2)}- `,
-        added: `${' '.repeat(currentDepth === 1 ? 2 : 3 * currentDepth + currentDepth - 2)}+ `,
-        changed: (sign) => `${' '.repeat(currentDepth === 1 ? 2 : 3 * currentDepth + currentDepth - 2)}${sign} `,
+    const rendered = Object.values(tree).reduce((acc, treeValue) => {
+      const {
+        name, status, type, value, valuePrev, children,
+      } = treeValue[0];
+      const getIndent = (property) => {
+        switch (property) {
+          case 'normal':
+            return ' '.repeat(4 * currentDepth);
+          case 'object':
+            return ' '.repeat(4 * (currentDepth + 1));
+          default:
+            return ' '.repeat(currentDepth === 1 ? 2 : 3 * currentDepth + currentDepth - 2);
+        }
       };
-      const nameAst = key;
-      const valueAst = JSON.stringify(value[0].value || value[0].valuePrev);
-      const statusAst = value[0].status;
+      const indents = {
+        unchanged: getIndent('normal'),
+        removed: `${getIndent()}- `,
+        added: `${getIndent()}+ `,
+        changed: (sign) => `${getIndent()}${sign} `,
+      };
       const stringify = (str) => (typeof str === 'string' ? str.replace(/(^")|("$)/g, '') : str);
       const formatObj = (item) => {
-        const jsonItem = JSON.parse(item);
-        if (Array.isArray(jsonItem)) {
-          const allItems = jsonItem.map((itemKey) => `${' '.repeat(4 * (currentDepth + 1))}${itemKey}`);
-          return `[\n${allItems.join('\n')}\n${' '.repeat(4 * currentDepth)}]`;
+        const parsedItem = JSON.parse(item);
+        if (isArray(parsedItem)) {
+          const formatedArr = parsedItem.map((el) => `${getIndent('object')}${el}`);
+          return `[\n${formatedArr.join('\n')}\n${getIndent('normal')}]`;
         }
-        if (jsonItem instanceof Object) {
-          const itemKeys = Object.keys(jsonItem);
-          const allItems = itemKeys.map((itemKey) => `${' '.repeat(4 * (currentDepth + 1))}${itemKey}: ${stringify(jsonItem[itemKey])}`);
-          return `{\n${allItems.join('\n')}\n${' '.repeat(4 * currentDepth)}}`;
+        if (isObject(parsedItem)) {
+          const formatedObj = Object.keys(parsedItem).map((objKey) => `${getIndent('object')}${objKey}: ${stringify(parsedItem[objKey])}`);
+          return `{\n${formatedObj.join('\n')}\n${getIndent('normal')}}`;
         }
         return stringify(item);
       };
-      if (value[0].type === 'children') {
-        return [...acc, `${mapStatus[statusAst]}${nameAst}: {\n${iter(value[0].children, currentDepth)}\n${' '.repeat(4 * currentDepth)}}`];
+
+      if (type === 'children') {
+        return [...acc, `${indents[status]}${name}: {\n${iterAst(children, currentDepth)}\n${getIndent('normal')}}`];
       }
-      if (statusAst === 'changed') {
-        return [...acc, `${mapStatus[statusAst]('+')}${nameAst}: ${formatObj(JSON.stringify(value[0].valuePrev))}`, `${mapStatus[statusAst]('-')}${nameAst}: ${formatObj(JSON.stringify(value[0].value))}`];
+
+      if (status === 'changed') {
+        return [...acc, `${indents[status]('+')}${name}: ${formatObj(JSON.stringify(valuePrev))}`,
+          `${indents[status]('-')}${name}: ${formatObj(JSON.stringify(value))}`];
       }
-      return [...acc, `${mapStatus[statusAst]}${nameAst}: ${formatObj(valueAst)}`];
+      return [...acc, `${indents[status]}${name}: ${formatObj(JSON.stringify(value || valuePrev))}`];
     }, []);
-    return `${res.join('\n')}`;
+
+    return `${rendered.join('\n')}`;
   };
 
-  return `{\n${iter(ast, 0)}\n}`;
+  return `{\n${iterAst(ast, 0)}\n}`;
 };
 
 export default (pathToFile1, pathToFile2) => {
