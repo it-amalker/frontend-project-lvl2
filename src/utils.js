bbbbbdf80fce177f18bdbc6ba60fa2cbae68d43f
amalker <it.amalker@yandex.ru>
2020-01-28 18:13:16 +0300

    add new function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
 export const isObject = (...items) => items.every((obj) => obj instanceof Object);
 export const isArray = (...items) => items.every((arr) => Array.isArray(arr));
+export const isExactlyObject = (...items) => items.every((obj) => isObject(obj) && !isArray(obj));
+export const compareArray = (arr1, arr2) => arr1.every((el) => arr2.includes(el));
 export const stringify = (str) => (typeof str === 'string' ? str.replace(/(^")|("$)/g, '') : str);
