5d4f7a63feac55d11f2373ea6a22ba424053a9dc
amalker <it.amalker@yandex.ru>
2020-01-28 15:20:39 +0300

    refactor default renderer

--- src/formatters/defaultRender.js
+++ src/formatters/defaultRender.js
@@ -3,55 +3,54 @@ import { isArray, isObject, stringify } from '../utils';
 export default (ast) => {
   const iterAst = (tree, depth) => {
     const currentDepth = depth + 1;
+    const renderIndent = (status) => {
+      const defaultIndent = ' '.repeat(4 * currentDepth);
+      const objectIndent = ' '.repeat(4 * (currentDepth + 1));
+      const nestedIndent = ' '.repeat(currentDepth === 1 ? 2 : 3 * currentDepth + currentDepth - 2);
+      switch (status) {
+        case 'unchanged':
+          return defaultIndent;
+        case 'removed':
+          return `${nestedIndent}- `;
+        case 'added':
+          return `${nestedIndent}+ `;
+        case 'changed':
+          return `${nestedIndent}`;
+        case 'object':
+          return objectIndent;
+        default:
+          return nestedIndent;
+      }
+    };
+    const renderObj = (item) => {
+      if (isArray(item)) {
+        const mappedArr = item.map((el) => `${renderIndent('object')}${el}`);
+        return `[\n${mappedArr.join('\n')}\n${renderIndent('unchanged')}]`;
+      }
+      if (isObject(item)) {
+        const mappedObj = Object.entries(item).map(([key, value]) => `${renderIndent('object')}${key}: ${stringify(value)}`);
+        return `{\n${mappedObj.join('\n')}\n${renderIndent('unchanged')}}`;
+      }
+      return stringify(JSON.stringify(item));
+    };
     const rendered = Object.values(tree).reduce((acc, treeValue) => {
       const {
         name, status, type, value, valuePrev, children,
       } = treeValue[0];
-      const renderIndent = (property) => {
-        const defaultIndent = ' '.repeat(4 * currentDepth);
-        const objectIndent = ' '.repeat(4 * (currentDepth + 1));
-        const nestedIndent = ' '.repeat(currentDepth === 1 ? 2 : 3 * currentDepth + currentDepth - 2);
-        switch (property) {
-          case 'unchanged':
-            return defaultIndent;
-          case 'removed':
-            return `${nestedIndent}- `;
-          case 'added':
-            return `${nestedIndent}+ `;
+      const renderString = (treeStatus) => {
+        switch (treeStatus) {
           case 'changed':
-            return (sign) => `${nestedIndent}${sign} `;
-          case 'object':
-            return objectIndent;
+            return [...acc, `${renderIndent(status)}+ ${name}: ${renderObj(valuePrev)}`,
+              `${renderIndent(status)}- ${name}: ${renderObj(value)}`];
           default:
-            return nestedIndent;
+            return [...acc, `${renderIndent(status)}${name}: ${renderObj(value || valuePrev)}`];
         }
       };
-      const formatObj = (item) => {
-        const parsedItem = JSON.parse(item);
-        if (isArray(parsedItem)) {
-          const formatedArr = parsedItem.map((el) => `${renderIndent('object')}${el}`);
-          return `[\n${formatedArr.join('\n')}\n${renderIndent('unchanged')}]`;
-        }
-        if (isObject(parsedItem)) {
-          const formatedObj = Object.keys(parsedItem).map((objKey) => `${renderIndent('object')}${objKey}: ${stringify(parsedItem[objKey])}`);
-          return `{\n${formatedObj.join('\n')}\n${renderIndent('unchanged')}}`;
-        }
-        return stringify(item);
-      };
-
-      if (type === 'children') {
-        return [...acc, `${renderIndent(status)}${name}: {\n${iterAst(children, currentDepth)}\n${renderIndent('unchanged')}}`];
-      }
-
-      if (status === 'changed') {
-        return [...acc, `${renderIndent(status)('+')}${name}: ${formatObj(JSON.stringify(valuePrev))}`,
-          `${renderIndent(status)('-')}${name}: ${formatObj(JSON.stringify(value))}`];
-      }
-      return [...acc, `${renderIndent(status)}${name}: ${formatObj(JSON.stringify(value || valuePrev))}`];
+      return type === 'children'
+        ? [...acc, `${renderIndent(status)}${name}: {\n${iterAst(children, currentDepth)}\n${renderIndent('unchanged')}}`]
+        : renderString(status);
     }, []);
-
     return `${rendered.join('\n')}`;
   };
-
   return `{\n${iterAst(ast, 0)}\n}`;
 };
