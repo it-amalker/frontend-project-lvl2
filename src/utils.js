e9af9ec721865771ae072993823d15fb07e609b2
amalker <it.amalker@yandex.ru>
2020-01-29 20:25:34 +0300

    add convertNums func for ini parser

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,15 @@ export const isArray = (...items) => items.every((arr) => Array.isArray(arr));
 export const isExactlyObject = (...items) => items.every((obj) => isObject(obj) && !isArray(obj));
 export const compareArray = (arr1, arr2) => arr1.every((el) => arr2.includes(el));
 export const stringify = (str) => (typeof str === 'string' ? str.replace(/(^")|("$)/g, '') : str);
+export const convertNums = (obj) => {
+  const result = Object.entries(obj).reduce((acc, [key, value]) => {
+    if (isObject(value)) {
+      convertNums(value);
+    }
+    if (/^\d+$/.test(value)) {
+      acc[key] = Number(value);
+    }
+    return acc;
+  }, obj);
+  return result;
+};
