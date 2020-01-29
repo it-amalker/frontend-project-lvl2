08174bf0a4c2a1a397ca4e87e3d7d255199edec5
amalker <it.amalker@yandex.ru>
2020-01-29 20:21:25 +0300

    add json parser

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import parse from './parsers';
 import diff from './diff';
 import defaultRender from './formatters/defaultRender';
 import plainRender from './formatters/plain';
-
+import jsonRender from './formatters/json';
 
 export default (pathToFile1, pathToFile2, format) => {
   const getRenderer = (outputFormat) => {
     switch (outputFormat) {
       case 'plain':
         return plainRender;
-      // case 'json':
-        // return json;
+      case 'json':
+        return jsonRender;
       default:
         return defaultRender;
     }
   };
   const render = getRenderer(format);
-
   return render(diff(parse(pathToFile1), parse(pathToFile2)));
 };
