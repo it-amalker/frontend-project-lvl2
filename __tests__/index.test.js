f5f0e84328579ff9326a5d4e1c9ca6c1c51e82da
amalker <it.amalker@yandex.ru>
2020-02-01 12:50:10 +0300

    refactor tests, remove similar test checks

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,44 +1,35 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
 
-const path = `${__dirname}/../__fixtures__/`;
-
-const default1 = String(fs.readFileSync(`${path}expected1`));
-const default2 = String(fs.readFileSync(`${path}expected2`));
-const default3 = String(fs.readFileSync(`${path}expected3`));
-const default4 = String(fs.readFileSync(`${path}expected4`));
-const plain1 = String(fs.readFileSync(`${path}expected1-plain`));
-const plain2 = String(fs.readFileSync(`${path}expected2-plain`));
-const plain3 = String(fs.readFileSync(`${path}expected3-plain`));
-const plain4 = String(fs.readFileSync(`${path}expected4-plain`));
-const json1 = String(fs.readFileSync(`${path}expected1-json`));
-const json2 = String(fs.readFileSync(`${path}expected2-json`));
-const json3 = String(fs.readFileSync(`${path}expected3-json`));
-const json4 = String(fs.readFileSync(`${path}expected4-json`));
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe.only.each`
-  format       |  file1                 | file2                 | normal       | plain      |  json
-  ${'JSON #1'} |  ${`${path}json1`}     | ${`${path}json2`}     | ${default1}  | ${plain1}  |  ${json1}
-  ${'JSON #2'} |  ${`${path}json3`}     | ${`${path}json4`}     | ${default2}  | ${plain2}  |  ${json2}
-  ${'JSON #3'} |  ${`${path}json5`}     | ${`${path}json6`}     | ${default3}  | ${plain3}  |  ${json3}
-  ${'YAML #1'} |  ${`${path}yaml1.yml`} | ${`${path}yaml2.yml`} | ${default1}  | ${plain1}  |  ${json1}
-  ${'YAML #2'} |  ${`${path}yaml3.yml`} | ${`${path}yaml4.yml`} | ${default2}  | ${plain2}  |  ${json2}
-  ${'YAML #3'} |  ${`${path}yaml5.yml`} | ${`${path}yaml6.yml`} | ${default3}  | ${plain3}  |  ${json3}
-  ${'INI #1'}  |  ${`${path}conf1.ini`} | ${`${path}conf2.ini`} | ${default4}  | ${plain4}  |  ${json4}
-  ${'INI #2'}  |  ${`${path}conf3.ini`} | ${`${path}conf4.ini`} | ${default2}  | ${plain2}  |  ${json2}
-  ${'INI #3'}  |  ${`${path}conf5.ini`} | ${`${path}conf6.ini`} | ${default3}  | ${plain3}  |  ${json3}
+  format    | pathToFile1      | pathToFile2
+  ${'JSON'} | ${'before.json'} | ${'after.json'}
+  ${'YAML'} | ${'before.yml'}  | ${'after.yml'}
+  ${'INI'}  | ${'before.ini'}  | ${'after.ini'}
 `('TESTS', ({
-  format, file1, file2, normal, plain, json,
+  format, pathToFile1, pathToFile2,
 }) => {
+  const before = getFixturePath(pathToFile1);
+  const after = getFixturePath(pathToFile2);
+
+  const isIniFormat = () => (format === 'INI' ? '-ini' : '');
+  const defaultOutput = readFile(`result${isIniFormat()}`);
+  const plainOutput = readFile(`result${isIniFormat()}-plain`);
+  const jsonOutput = readFile(`result${isIniFormat()}.json`);
+
   test(`${format} (default)`, () => {
-    expect(genDiff(file1, file2)).toBe(normal);
+    expect(genDiff(before, after)).toBe(defaultOutput);
   });
 
   test(`${format} (plain)`, () => {
-    expect(genDiff(file1, file2, 'plain')).toBe(plain);
+    expect(genDiff(before, after, 'plain')).toBe(plainOutput);
   });
 
   test(`${format} (json)`, () => {
-    expect(genDiff(file1, file2, 'json')).toBe(json);
+    expect(genDiff(before, after, 'json')).toBe(jsonOutput);
   });
 });
