cf15d11afa6eba98bb8dd5e4310927956b083798
amalker <it.amalker@yandex.ru>
2020-02-04 20:13:43 +0300

    start to refactor module, change ast structure

--- src/diff.js
+++ src/diff.js
@@ -1,55 +1,47 @@
 import _ from 'lodash';
-import { isArray, compareArray, isExactlyObject } from './utils';
+import { isEquilArrays } from './utils';
 
 const genDiff = (object1, object2) => {
-  const commonKeys = _.uniq(Object.keys(object1).concat(Object.keys(object2)));
-  const ast = commonKeys.sort().reduce((acc, key) => {
-    const value1 = object1[key];
-    const value2 = object2[key];
-    const isValuesAreObjects = isExactlyObject(value1, value2);
-    const buildAstNode = (obj1, obj2) => {
-      const checkStatus = (arg1, arg2) => {
-        if (isArray(arg1, arg2)) {
-          return compareArray(value1, value2) ? 'unchanged' : 'changed';
-        }
-        return arg1 === arg2 ? 'unchanged' : 'changed';
-      };
-      const nodeProperties = [
-        {
-          type: isValuesAreObjects ? 'children' : 'regular',
-          status: isValuesAreObjects ? 'unchanged' : checkStatus(value1, value2),
-          value: isValuesAreObjects ? '' : value1,
-          valuePrev: isValuesAreObjects ? '' : value2,
-          check: (arg1, arg2) => arg1 && arg2,
-        },
-        {
-          status: 'removed',
-          value: value1,
-          check: (arg1, arg2) => arg1 && !arg2,
-        },
-        {
-          status: 'added',
-          valuePrev: value2,
-          check: (arg1, arg2) => !arg1 && arg2,
-        },
-      ];
-      const checkConditions = (arg1, arg2) => nodeProperties.find(({ check }) => check(arg1, arg2));
-      const {
-        type = 'regular', status, value = '', valuePrev = '',
-      } = checkConditions(_.has(obj1, key), _.has(obj2, key));
+  const commonKeys = _.union(_.keys(object1), _.keys(object2));
+  const ast = commonKeys.sort().map((key) => {
+    const node = {
+      name: key,
+      type: 'regular',
+      status: '',
+      valueBefore: '',
+      valueAfter: '',
+    };
+    const buildNode = (obj1, obj2) => {
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+      if (value1 && !value2 && value2 !== '') {
+        return { ...node, status: 'removed', valueBefore: value1 };
+      }
+      if (!value1 && value2 && value1 !== '') {
+        return { ...node, status: 'added', valueAfter: value2 };
+      }
+      if (value1 === value2 && !_.isObject(value1) && !_.isObject(value2)) {
+        return {
+          ...node, status: 'unchanged', valueBefore: value1, valueAfter: value2,
+        };
+      }
+      if (_.isArray(value1) && _.isArray(value2)) {
+        const statusResult = isEquilArrays(value1, value2) ? 'unchanged' : 'changed';
+        return {
+          ...node, status: statusResult, valueBefore: value1, valueAfter: value2,
+        };
+      }
+      if (_.isObject(value1) && _.isObject(value2)) {
+        return {
+          ...node, type: 'children', status: 'unchanged', children: genDiff(value1, value2),
+        };
+      }
       return {
-        name: key, type, status, value, valuePrev,
+        ...node, status: 'changed', valueBefore: value1, valueAfter: value2,
       };
     };
-    const node = buildAstNode(object1, object2);
-    const astNode = node.type === 'children'
-      ? { ...node, children: genDiff(value1, value2) } : node;
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    acc[key].push(astNode);
-    return acc;
-  }, {});
+    return buildNode(object1, object2);
+  });
   return ast;
 };
 
