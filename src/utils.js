16b8adf30f8461159d97b19ffeb00963d49ed703
amalker <it.amalker@yandex.ru>
2020-02-04 20:12:45 +0300

    rename function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const isObject = (...items) => items.every((obj) => obj instanceof Object);
 export const isArray = (...items) => items.every((arr) => Array.isArray(arr));
 export const isExactlyObject = (...items) => items.every((obj) => isObject(obj) && !isArray(obj));
-export const compareArray = (arr1, arr2) => arr1.every((el) => arr2.includes(el));
+export const isEquilArrays = (arr1, arr2) => arr1.every((el) => arr2.includes(el));
 export const stringify = (str) => (typeof str === 'string' ? str.replace(/(^")|("$)/g, '') : str);
 export const convertNums = (obj) => {
   const result = Object.entries(obj).reduce((acc, [key, value]) => {
