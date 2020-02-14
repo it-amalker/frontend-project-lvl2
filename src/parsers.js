ee86da2d3bec9b2e6c9cd31df9410b9e2fe51fe1
amalker <it.amalker@yandex.ru>
2020-02-14 16:29:46 +0300

    refactor module

--- src/parsers.js
+++ src/parsers.js
@@ -2,19 +2,20 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 import { replaceNumericStrings } from './utils';
 
+const getParser = (dataType) => {
+  switch (dataType) {
+    case 'json':
+      return JSON.parse;
+    case 'yml':
+      return yaml.safeLoad;
+    case 'ini':
+      return ini.parse;
+    default:
+      throw new Error(`Unsupported ${dataType} data type`);
+  }
+};
+
 export default (data, dataType) => {
-  const getParser = (typeOfData) => {
-    switch (typeOfData) {
-      case 'json':
-        return JSON.parse;
-      case 'yml':
-        return yaml.safeLoad;
-      case 'ini':
-        return ini.parse;
-      default:
-        throw new Error('Unsupported data type');
-    }
-  };
   const parse = getParser(dataType);
   return dataType === 'ini' ? replaceNumericStrings(parse(data)) : parse(data);
 };
