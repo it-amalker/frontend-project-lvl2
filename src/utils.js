3d2d7575a72fc853881fe74c890e37eb94f9451e
amalker <it.amalker@yandex.ru>
2020-03-02 18:47:05 +0300

    add predicate function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
 
+export const isObjectAndNotArray = (value1, value2) => (
+  _.isObject(value1) && _.isObject(value2) && !_.isArray(value1) && !_.isArray(value2)
+);
 export const stringify = (str) => (typeof str === 'string' ? str.replace(/(^")|("$)/g, '') : str);
 export const replaceNumericStrings = (obj) => {
   const result = Object.entries(obj).reduce((acc, [key, value]) => {
