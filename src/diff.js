0830691e130e43da330c44a475548a438ec30dc7
amalker <it.amalker@yandex.ru>
2020-04-09 15:41:23 +0300

    small refactoring

--- src/diff.js
+++ src/diff.js
@@ -5,16 +5,24 @@ const buildAst = (object1, object2) => {
   const commonKeys = _.union(_.keys(object1), _.keys(object2));
   const buildNode = (key, obj1, obj2) => {
     const node = {
-      name: key, type: 'unchanged', valueBefore: '', valueAfter: '',
+      name: key,
+      type: 'unchanged',
+      valueBefore: '',
+      valueAfter: '',
     };
-    const [value1, value2] = [obj1[key], obj2[key]];
-    const [isKeyInObj1, isKeyInObj2] = [_.has(obj1, key), _.has(obj2, key)];
+
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    const isKeyInObj1 = _.has(obj1, key);
+    const isKeyInObj2 = _.has(obj2, key);
+
     const nodeTypes = {
       removed: { check: isKeyInObj1 && !isKeyInObj2 },
       added: { check: !isKeyInObj1 && isKeyInObj2 },
       children: { check: isObjectAndNotArray(value1, value2) },
       unchanged: { check: _.isEqual(value1, value2) },
     };
+
     const currentNodeType = _.findKey(nodeTypes, 'check') || 'changed';
     const nodeByNodeType = {
       removed: () => ({ ...node, type: 'removed', valueBefore: value1 }),
@@ -25,6 +33,7 @@ const buildAst = (object1, object2) => {
         ...node, type: 'changed', valueBefore: value1, valueAfter: value2,
       }),
     };
+
     return nodeByNodeType[currentNodeType]();
   };
   return commonKeys.sort().map((currentKey) => buildNode(currentKey, object1, object2));
